7483cd5343f2df012dc9eb01d4147e1ac19a9a4f
Matheus Reis de Oliveira <47448206+matheusoreis@users.noreply.github.com>
2025-08-06 14:03:29 -0300

    Upload do projeto base

---
+++ src/plugins/index.ts
@@ -0,0 +1,8 @@
+import vuetify from "./vuetify";
+import router from "../router";
+
+import type { App } from "vue";
+
+export function registerPlugins(app: App) {
+  app.use(vuetify).use(router);
+}
